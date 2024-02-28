3cdd7172227c7043dcbb0675eea136cd62048c4b
Afonso Azaruja <afonso.azaruja@tecnico.ulisboa.pt>
2024-02-28 09:51:25 +0000

    Created Participation DTO closes #29

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/dto/ParticipationDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/dto/ParticipationDto.java
@@ -1,4 +1,43 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto;
 
+import jakarta.activation.DataHandler;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation;
+
+import java.time.LocalDateTime;
+
 public class ParticipationDto {
+    private Integer rating;
+    private LocalDateTime acceptanceDate;
+
+    public ParticipationDto() {
+    }
+
+    public ParticipationDto(Participation participation) {
+        setRating(participation.getRating());
+        setAcceptanceDate(participation.getAcceptanceDate());
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+    
+    public LocalDateTime getAcceptanceDate() {
+        return acceptanceDate;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public void setAcceptanceDate(LocalDateTime acceptanceDate) {
+        this.acceptanceDate = acceptanceDate;
+    }
+
+    @Override
+    public String toString() {
+        return "ParticipationDto{" +
+                "rating=" + rating +
+                ", acceptanceDate='" + acceptanceDate +
+                '}';
+    }
 }
