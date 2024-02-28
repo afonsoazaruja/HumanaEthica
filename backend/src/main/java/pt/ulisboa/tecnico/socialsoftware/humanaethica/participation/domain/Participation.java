4b0b3cd2e213f3ff9578f7884cdeadc92c8e58f7
Henrique Silva <henriqueapsilva@tecnico.ulisboa.pt>
2024-02-28 12:58:46 +0000

    feat: Implemented relations between Activity and Participation. closes #26

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/domain/Participation.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/domain/Participation.java
@@ -1,16 +1,36 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain;
 
+import jakarta.persistence.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto;
 
 import java.time.LocalDateTime;
-
+@Entity
+@Table(name = "participation")
 public class Participation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private Integer rating;
     private LocalDateTime acceptanceDate;
 
-    public Participation(Integer rating, LocalDateTime acceptanceDate) {
+    @ManyToOne
+    private Activity activity;
+
+    public Participation() {
+
+    }
+    public Participation(Integer rating, LocalDateTime acceptanceDate, Activity activity) {
         this.rating = rating;
         this.acceptanceDate = acceptanceDate;
+        this.activity = activity;
+        addParticipation();
+    }
+
+
+    public Integer getId() {
+        return id;
     }
 
     public Integer getRating() {
@@ -27,5 +47,16 @@ public class Participation {
 
     public void setAcceptanceDate(LocalDateTime acceptanceDate) {
         this.acceptanceDate = acceptanceDate;
+    }
+
+    public void addParticipation(){activity.addParticipation(this);}
+
+    public void removeParticipation(){activity.removeParticipation(this);}
+    public Activity getActivity() {
+        return this.activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
     }
 }
