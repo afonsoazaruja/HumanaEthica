26ade261b73e45cd05700804a9f29280ef3a68cb
Afonso Azaruja <afonso.azaruja@tecnico.ulisboa.pt>
2024-03-02 17:10:58 +0000

    feat: Created test case for successful creation of Participation. closes #34

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/domain/CreateParticipationMethodTest.groovy
@@ -0,0 +1,57 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.dto.ActivityDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.theme.domain.Theme
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler
+import spock.lang.Unroll
+
+import java.time.LocalDateTime
+
+@DataJpaTest
+class CreateParticipationMethodTest extends SpockTest {
+    Activity activity = Mock()
+    Participation otherParticipation = Mock()
+    Volunteer volunteer = Mock()
+    def participationDto
+
+    @Unroll
+    def "create participation with activity and volunteer has another participation"() {
+        given:
+        otherParticipation.getVolunteer().getName() >> USER_2_NAME
+        activity.getParticipations() >> [otherParticipation]
+        activity.getName() >> ACTIVITY_NAME_1
+        activity.getRegion() >> ACTIVITY_REGION_1
+        activity.getParticipantsNumberLimit() >> 2
+        activity.getDescription() >> ACTIVITY_DESCRIPTION_1
+        activity.getApplicationDeadline() >> ONE_DAY_AGO
+        volunteer.getName() >> USER_1_NAME
+
+        and: "participation info"
+        participationDto = new ParticipationDto()
+        participationDto.rating = 10
+
+        when:
+        def result = new Participation(activity, volunteer, participationDto)
+
+        then: "check results"
+        result.getVolunteer().getName() == USER_1_NAME
+        result.getActivity().getName() == ACTIVITY_NAME_1
+        result.getRating() == 10
+        and: "invocations"
+        1 * activity.addParticipation(_)
+        1 * volunteer.addParticipation(_)
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+}
