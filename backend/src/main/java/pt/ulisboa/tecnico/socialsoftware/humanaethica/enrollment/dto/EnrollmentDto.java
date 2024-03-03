072f7c83bc2b8191243626feb0bafd202120246f
Fabio Neto <fabiogvdneto@tecnico.ulisboa.pt>
2024-03-03 14:59:59 +0000

    feat: implement enrollment constructor
    closes #44

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/dto/EnrollmentDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/dto/EnrollmentDto.java
@@ -3,12 +3,10 @@ package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler;
 
-import java.time.LocalDateTime;
-
 public class EnrollmentDto {
     private Integer id;
     private String motivation;
-    private String dateTime;
+    private String enrollmentDateTime;
 
     public EnrollmentDto(){
     }
@@ -16,27 +14,39 @@ public class EnrollmentDto {
     public EnrollmentDto(Enrollment enrollment){
         setId(enrollment.getId());
         setMotivation(enrollment.getMotivation());
-        setDateTime(DateHandler.toISOString(enrollment.getDateTime()));
+        setEnrollmentDateTime(DateHandler.toISOString(enrollment.getDateTime()));
     }
 
-    public void setId(Integer id) {this.id = id;}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public Integer getId() {return id;}
+    public Integer getId() {
+        return id;
+    }
 
-    public void setMotivation(String motivation) {this.motivation = motivation;}
+    public void setMotivation(String motivation) {
+        this.motivation = motivation;
+    }
 
-    public String getMotivation() {return motivation;}
+    public String getMotivation() {
+        return motivation;
+    }
 
-    public void setDateTime(String dateTime) {this.dateTime = dateTime;}
+    public void setEnrollmentDateTime(String enrollmentDateTime) {
+        this.enrollmentDateTime = enrollmentDateTime;
+    }
 
-    public String getDateTime() {return dateTime;}
+    public String getEnrollmentDateTime() {
+        return enrollmentDateTime;
+    }
 
     @Override
     public String toString() {
         return "EnrollmentDTO{" +
                 "id=" + id +
                 ", motivation='" + motivation + '\'' +
-                ", dateTime=" + dateTime +
+                ", dateTime=" + enrollmentDateTime +
                 "}";
     }
 }
