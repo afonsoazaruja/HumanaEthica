8a79d0355dbfbb8aba0b10a55da547a1fad8b771
João Bravo <joaocgbravo@tecnico.ulisboa.pt>
2024-03-05 18:53:59 +0000

    feat: implemented POST Controller closes #79

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentController.java
@@ -1,14 +1,21 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment;
 
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto.AssessmentDto;
 
+import org.springframework.web.bind.annotation.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto.AssessmentDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
+
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -22,5 +29,12 @@ public class AssessmentController {
     @GetMapping("/{institutionId}")
     public List<AssessmentDto> getInstitutionAssessments(@PathVariable Integer institutionId) {
         return assessmentService.getAssessmentsByInstitution(institutionId);
+    }
+
+    @PostMapping()
+    @PreAuthorize("(hasRole('ROLE_VOLUNTEER'))")
+    public AssessmentDto createAssessment(Principal principal, @PathVariable Integer institutionId, @Valid @RequestBody AssessmentDto assessmentDto){
+        int userId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
+        return assessmentService.createAssessment(userId, institutionId, assessmentDto);
     }
 }
