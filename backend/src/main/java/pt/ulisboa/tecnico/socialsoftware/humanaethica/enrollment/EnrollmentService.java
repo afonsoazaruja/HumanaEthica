88a220f4136152d1e1f8a07416c9c777bcdb01aa
Fabio Neto <fabiogvdneto@gmail.com>
2024-03-05 21:34:21 +0000

    feat: implemented enrollment service Create
    closes #73

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/EnrollmentService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/EnrollmentService.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.repository.ActivityRepository;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto.EnrollmentDto;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.repository.EnrollmentRepository;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.repository.UserRepository;
 
 import java.util.List;
@@ -29,5 +32,20 @@ public class EnrollmentService {
         return enrollmentRepository.getEnrollmentsByActivity(activityId).stream()
                 .map(EnrollmentDto::new)
                 .toList();
+    }
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public EnrollmentDto createEnrollment(Integer userId, Integer activityId, EnrollmentDto enrollmentDto) {
+        if (userId == null) throw new HEException(ErrorMessage.USER_NOT_FOUND);
+        if (activityId == null) throw new HEException(ErrorMessage.ACTIVITY_NOT_FOUND);
+
+        Volunteer volunteer = (Volunteer) userRepository.findById(userId)
+                .orElseThrow(() -> new HEException(ErrorMessage.USER_NOT_FOUND, userId));
+        Activity activity = activityRepository.findById(activityId)
+                .orElseThrow(() -> new HEException(ErrorMessage.ACTIVITY_NOT_FOUND, activityId));
+
+        Enrollment enrollment = new Enrollment(activity, volunteer, enrollmentDto);
+
+        return new EnrollmentDto(enrollmentRepository.save(enrollment));
     }
 }
