c99822131b8e11a9a7299ec462ee2988b3d58c16
Pedro <pedrorebeloalmeida@tecnico.ulisboa.pt>
2024-03-06 00:21:34 +0000

    feat: test for service get input verification closes #56

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/service/GetAssessmentsServiceTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/service/GetAssessmentsServiceTest.groovy
@@ -7,6 +7,8 @@ import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.AssessmentService
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.domain.Assessment
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.theme.domain.Theme
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.User
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer
@@ -14,9 +16,17 @@ import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler
 
 @DataJpaTest
 class GetAssessmentsServiceTest extends SpockTest {
+    public static final String EXIST = "exist"
+    public static final String NO_EXIST = "noExist"
+
+    def institution
+
+    def setup() {
+        institution = institutionService.getDemoInstitution()
+    }
+
     def "get two assessments"() {
         given: "an institution with a completed activity and assessment info"
-        def institution = institutionService.getDemoInstitution()
         def activityDto = createActivityDto(ACTIVITY_NAME_1,ACTIVITY_REGION_1,1,ACTIVITY_DESCRIPTION_1,
                 DateHandler.now().minusDays(3),TWO_DAYS_AGO,ONE_DAY_AGO,null)
         def themes = new ArrayList<>()
@@ -45,6 +55,28 @@ class GetAssessmentsServiceTest extends SpockTest {
         result.size() == 2
         result.get(0).review == ASSESSMENT_REVIEW_1
         result.get(1).review == ASSESSMENT_REVIEW_2
+    }
+
+    def 'invalid argument: institutionId=#institutionId'() {
+        when:
+        assessmentService.getAssessmentsByInstitution(getInstitutionId(institutionId))
+
+        then:
+        def error = thrown(HEException)
+        error.getErrorMessage() == errorMessage
+
+        where:
+        institutionId   || errorMessage
+        null            || ErrorMessage.INSTITUTION_NOT_FOUND
+        NO_EXIST        || ErrorMessage.INSTITUTION_NOT_FOUND
+    }
+
+    def getInstitutionId(institutionId){
+        if (institutionId == EXIST)
+            return institution.id
+        else if (institutionId == NO_EXIST)
+            return 222
+        return null
     }
 
     @TestConfiguration
