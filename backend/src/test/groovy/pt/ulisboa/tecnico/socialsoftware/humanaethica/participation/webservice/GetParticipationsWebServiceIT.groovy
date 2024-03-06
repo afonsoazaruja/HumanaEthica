de783d87ff45c7c46245e4e5d0be8e0f96aa2221
Afonso Azaruja <afonso.azaruja@tecnico.ulisboa.pt>
2024-03-06 11:14:23 +0000

    feat: implemented test GET controller (Success) closes #88

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/webservice/GetParticipationsWebServiceIT.groovy
@@ -0,0 +1,74 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.webservice
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.client.WebClient
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.dto.ActivityDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.domain.Participation
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.theme.domain.Theme
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.User
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GetParticipationsWebServiceIT extends SpockTest {
+    @LocalServerPort
+    private int port
+
+    def activity
+    def participation
+    def volunteerId
+
+    def setup() {
+        deleteAll()
+
+        webClient = WebClient.create("http://localhost:" + port)
+        headers = new HttpHeaders()
+        headers.setContentType(MediaType.APPLICATION_JSON)
+
+        def institution = institutionService.getDemoInstitution()
+        given: "activity info"
+        def activityDto = createActivityDto(ACTIVITY_NAME_1,ACTIVITY_REGION_1,2,ACTIVITY_DESCRIPTION_1,
+                ONE_DAY_AGO,IN_TWO_DAYS,IN_THREE_DAYS,null)
+        and: "a theme"
+        def themes = new ArrayList<>()
+        themes.add(createTheme(THEME_NAME_1, Theme.State.APPROVED,null))
+        and: "an activity"
+        activity = new Activity(activityDto, institution, themes)
+        activityRepository.save(activity)
+        and: "a volunteer"
+        def volunteer = new Volunteer(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL, AuthUser.Type.NORMAL, User.State.SUBMITTED)
+        userRepository.save(volunteer)
+        volunteerId = volunteer.getId()
+        and: "a participation"
+        def participationDto = createParticipationDto(RATING_10, volunteer.getId())
+        participation = new Participation(activity, volunteer, participationDto)
+        participationRepository.save(participation)
+    }
+
+    def "get participations"() {
+        when:
+        def response = webClient.get()
+                .uri('/participations/' + activity.getId() + '/get')
+                .headers(httpHeaders -> httpHeaders.putAll(headers))
+                .retrieve()
+                .bodyToFlux(ParticipationDto.class)
+                .collectList()
+                .block()
+
+        then: "check response"
+        response.size() == 1
+        response.get(0).activity.getName() == ACTIVITY_NAME_1
+        response.get(0).volunteerId == volunteerId
+        response.get(0).rating == RATING_10
+        response.get(0).acceptanceDate != null
+        cleanup:
+        deleteAll()
+    }
+}
