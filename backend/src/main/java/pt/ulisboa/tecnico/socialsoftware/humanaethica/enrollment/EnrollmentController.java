879c2ed87c9ae6cf9400a5c94d718d668c93e30f
Tomás Macieira <tomasmacieira@tecnico.ulisboa.pt>
2024-03-07 00:07:46 +0000

    feat: implemented POST controller (enrollment) closes #98

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/EnrollmentController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/EnrollmentController.java
@@ -1,11 +1,22 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment;
 
-import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.slf4j.Logger;
+import org.springframework.security.access.prepost.PreAuthorize;
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto.EnrollmentDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
+import org.springframework.security.core.Authentication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.Principal;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/enrollments")
@@ -18,4 +29,13 @@ public class EnrollmentController {
     public List<EnrollmentDto> getEnrollments(@PathVariable Integer activityId) {
         return enrollmentService.getEnrollmentsByActivity(activityId);
     }
+    
+    @PostMapping()
+    @PreAuthorize("(hasRole('ROLE_VOLUNTEER'))")
+    public EnrollmentDto createEnrollment(Principal principal, @PathVariable Integer activityId, @Valid @RequestBody
+        EnrollmentDto enrollmentDto) {
+        int userId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
+        return enrollmentService.createEnrollment(userId, activityId, enrollmentDto);
+    }
+
 }
