79ec873fe9b6e77ac4ddad0ee11af45937515b65
Afonso Azaruja <afonso.azaruja@tecnico.ulisboa.pt>
2024-03-19 17:37:06 +0000

    feat: Add column volunteerName in class InstitutionActivityEnrollmentsView.
    Closes #118.

--- frontend/src/models/enrollment/Enrollment.ts
+++ frontend/src/models/enrollment/Enrollment.ts
@@ -5,6 +5,7 @@ export default class Enrollment {
   motivation!: string;
   activityId!: number;
   enrollmentDateTime!: string;
+  volunteerName!: string;
 
   constructor(jsonObj?: Enrollment) {
     if (jsonObj) {
@@ -12,6 +13,7 @@ export default class Enrollment {
       this.motivation = jsonObj.motivation;
       this.activityId = jsonObj.activityId;
       this.enrollmentDateTime = ISOtoString(jsonObj.enrollmentDateTime);
+      this.volunteerName = jsonObj.volunteerName;
     }
   }
 }
