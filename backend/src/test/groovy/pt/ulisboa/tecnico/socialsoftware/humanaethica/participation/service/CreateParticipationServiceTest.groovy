e0aad3c2756ee919c5fc635e3c774e91ab3d6d35
Henrique Silva <henriqueapsilva@tecnico.ulisboa.pt>
2024-03-22 11:19:15 +0000

    fix: added the correct file to the tests folder closes #157

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/service/CreateParticipationServiceTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/participation/service/CreateParticipationServiceTest.groovy
@@ -5,92 +5,100 @@ import org.springframework.boot.test.context.TestConfiguration
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.BeanConfiguration
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.participation.dto.ParticipationDto
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.theme.domain.Theme
 import spock.lang.Unroll
+
+import java.time.LocalDateTime
 
 @DataJpaTest
 class CreateParticipationServiceTest extends SpockTest {
-    public static final String EXIST = "exist"
-    public static final String NO_EXIST = "noExist"
-
-    def volunteerId
-    def activityId
+    public static final String EXIST = 'exist'
+    public static final String NO_EXIST = 'noExist'
+    def volunteer
+    def activity
 
     def setup() {
-        volunteerId = authUserService.loginDemoVolunteerAuth().getUser().getId()
         def institution = institutionService.getDemoInstitution()
-        def activityDto = createActivityDto(ACTIVITY_NAME_1,ACTIVITY_REGION_1,2,ACTIVITY_DESCRIPTION_1,
-                ONE_DAY_AGO,IN_TWO_DAYS,IN_THREE_DAYS,null)
-        def themes = new ArrayList<>()
-        themes.add(createTheme(THEME_NAME_1, Theme.State.APPROVED,null))
-        def activity = new Activity(activityDto, institution, themes)
+        volunteer = authUserService.loginDemoVolunteerAuth().getUser()
+
+        def activityDto = createActivityDto(ACTIVITY_NAME_1,ACTIVITY_REGION_1,3,ACTIVITY_DESCRIPTION_1,
+                TWO_DAYS_AGO, ONE_DAY_AGO, NOW,null)
+
+        activity = new Activity(activityDto, institution, new ArrayList<>())
         activityRepository.save(activity)
-        activityId = activity.getId()
     }
 
-    def "create participation"(){
-        given: "an participation dto"
-        def participationDto = createParticipationDto(RATING_10, volunteerId)
+    def 'create participant' () {
+        given:
+        def participationDto = new ParticipationDto()
+        participationDto.rating = 5
+        participationDto.volunteerId = volunteer.id
 
-        when: "a participation is created"
-        def result = participationService.createParticipation(activityId, participationDto)
+        when:
+        def result = participationService.createParticipation(activity.id, participationDto)
 
-        then: "the returned data is correct"
-        result.activity.getId() == activityId
-        result.rating == RATING_10
-        result.getVolunteerId() == volunteerId
-        result.acceptanceDate != null
-        and: "the activity is saved in the database"
+        then:
+        result.rating == 5
+        and:
         participationRepository.findAll().size() == 1
-        and: "the stored data is correct"
-        def storedParticipation = participationRepository.getParticipationByActivityId(activityId).get(0)
-        storedParticipation.activity.id == activityId
-        storedParticipation.rating == RATING_10
-        storedParticipation.getVolunteer().getId() == volunteerId
-        storedParticipation.acceptanceDate != null
-
+        def storedParticipation = participationRepository.findAll().get(0)
+        storedParticipation.rating == 5
+        storedParticipation.acceptanceDate.isBefore(LocalDateTime.now())
+        storedParticipation.activity.id == activity.id
+        storedParticipation.volunteer.id == volunteer.id
     }
 
     @Unroll
-    def 'invalid arguments: IDactivity=#IDactivity | volunteerId=#IDvolunteer'(){
-        given: "a participation dto"
-        def participationDto = createParticipationDto(RATING_1, getVolunteerId(IDvolunteer))
+    def 'invalid arguments: volunteerId=#volunteerId | activityId=#activityId'() {
+        given:
+        def participationDto = new ParticipationDto()
+        participationDto.rating = 5
+        participationDto.volunteerId = getVolunteerId(volunteerId)
 
         when:
-        participationService.createParticipation(getActivityId(IDactivity), participationDto)
+        participationService.createParticipation(getActivityId(activityId), getParticipationDto(participationValue,participationDto))
 
         then:
         def error = thrown(HEException)
         error.getErrorMessage() == errorMessage
-        and: "no participation is stored in the database"
+        and:
         participationRepository.findAll().size() == 0
 
         where:
-        IDactivity | IDvolunteer || errorMessage
-        NO_EXIST   | EXIST       || ErrorMessage.ACTIVITY_NOT_FOUND
-        null       | EXIST       || ErrorMessage.ACTIVITY_NOT_FOUND
-        EXIST      | NO_EXIST    || ErrorMessage.USER_NOT_FOUND
-        EXIST      | null        || ErrorMessage.USER_NOT_FOUND
+        volunteerId | activityId | participationValue || errorMessage
+        null        | EXIST      | EXIST              || ErrorMessage.USER_NOT_FOUND
+        NO_EXIST    | EXIST      | EXIST              || ErrorMessage.USER_NOT_FOUND
+        EXIST       | null       | EXIST              || ErrorMessage.ACTIVITY_NOT_FOUND
+        EXIST       | NO_EXIST   | EXIST              || ErrorMessage.ACTIVITY_NOT_FOUND
+        EXIST       | EXIST      | null               || ErrorMessage.PARTICIPATION_REQUIRES_INFORMATION
     }
 
-    def getActivityId(IDactivity){
-        if(IDactivity == EXIST)
-            return activityId
-        else if(IDactivity == NO_EXIST)
+    def getVolunteerId(volunteerId) {
+        if (volunteerId == EXIST)
+            return volunteer.id
+        else if (volunteerId == NO_EXIST)
             return 222
+        else
+            return null
+    }
+
+    def getActivityId(activityId) {
+        if (activityId == EXIST)
+            return activity.id
+        else if (activityId == NO_EXIST)
+            return 222
+        else
+            return null
+    }
+
+    def getParticipationDto(value, participationDto) {
+        if (value == EXIST) {
+            return participationDto
+        }
         return null
     }
-
-    def getVolunteerId(IDvolunteer){
-        if(IDvolunteer == EXIST)
-            return volunteerId
-        else if (IDvolunteer == NO_EXIST)
-            return 222
-        return null
-    }
-
 
     @TestConfiguration
     static class LocalBeanConfiguration extends BeanConfiguration {}
