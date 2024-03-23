100a4e96af8d69ab971766582b40be89d39f67ac
Henrique Silva <henriqueapsilva@tecnico.ulisboa.pt>
2024-03-23 18:00:19 +0000

    feat: Guarantee that the when a participation is created, it is sent in an event to InstitutionActivityEnrollmentsView which updates it's list of enrollments closes #132

--- frontend/src/services/RemoteServices.ts
+++ frontend/src/services/RemoteServices.ts
@@ -412,9 +412,9 @@ export default class RemoteServices {
       });
   }
 
-  static async registerParticipation(participation: Participation) {
+  static async registerParticipation(activityId: number, participation: Participation) {
     return httpClient
-      .post('/activities/{activityId}/participations', participation)
+      .post(`/activities/${activityId}/participations`,participation)
       .then((response) => {
         return new Participation(response.data);
       })
