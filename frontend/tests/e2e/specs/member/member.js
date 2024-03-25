2582a30c8eab6c71161e3527ae20d242c6fb69b0
Henrique Silva <henriqueapsilva@tecnico.ulisboa.pt>
2024-03-25 21:03:56 +0000

    fix: member test - back to the original closes #181

--- frontend/tests/e2e/specs/member/member.js
+++ frontend/tests/e2e/specs/member/member.js
@@ -1,46 +1,22 @@
 describe('Volunteer', () => {
   beforeEach(() => {
-    cy.demoMemberLogin()
+    cy.demoMemberLogin();
   });
 
-  afterEach(() => {
-    cy.logout();
-  });
+    afterEach(() => {
+      cy.logout();
+    });
 
-  it('close', () => {
-    cy.get('[data-cy="institution"]').click();
-    cy.get('[data-cy="members"]').click();
+    it('close', () => {
+      cy.get('[data-cy="institution"]').click();
+      cy.get('[data-cy="members"]').click();
 
-    cy.get('[data-cy="institution"]').click();
-    cy.get('[data-cy="themes"]').click();
+      cy.get('[data-cy="institution"]').click();
+      cy.get('[data-cy="themes"]').click();
 
-    cy.get('[data-cy="institution"]').click();
-    cy.get('[data-cy="activities"]').click();
-  });
-
-  it('select participation', () => {
-    const NUM_ACTIVITIES = '2';
-    const NUM_PARTICIPATIONS = '1';
-    const NUM_ENROLLMENTS = '2';
-
-    // go to activities table
-    cy.get('[data-cy="institution"]').click();
-    cy.get('[data-cy="activities"]').click();
-
-    // check results - 2 activities on the table
-    cy.get('[data-cy="memberActivitiesTable"] tbody tr')
-        .should('have.length', NUM_ACTIVITIES)
-
-    // check results - first activity has 1 participation
-    cy.get('[data-cy="memberActivitiesTable"] tbody tr')
-        .eq(0).children().eq(6).should('contain', NUM_PARTICIPATIONS)
-
-    // go to show enrollments from first activity
-    cy.get('[data-cy="showEnrollments"]').eq(0).click();
-
-    // check results - 2 enrollments on the table
-    cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
-        .should('have.length', NUM_ENROLLMENTS)
+      cy.get('[data-cy="institution"]').click();
+      cy.get('[data-cy="activities"]').click();
+    });
 
   });
-});
+
